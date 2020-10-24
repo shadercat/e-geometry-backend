fb49fcadcbc51d9cad952851fd2b37b6ab36e09a
Mariia Komarkova <Mariia_Komarkova@epam.com>
2020-10-24 11:29:37 +0300

    initial commit for e-geometry spring application

---
+++ src/main/java/ua/nure/egeometry/egeometry/ServiceLauncher.java
@@ -0,0 +1,13 @@
+package ua.nure.egeometry.egeometry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLauncher.class, args);
+    }
+
+}
